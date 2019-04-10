f95cb83a932bb9bc90f4b5233933844fa6047ffc
Anand Patel <anandpatel3@hotmail.com>
2019-04-10 15:27:40 -0400

    Update AutoWired.ts

--- src/spring-like/decorators/AutoWired.ts
+++ src/spring-like/decorators/AutoWired.ts
@@ -1,11 +1,14 @@
-export function AutoWired(name: string = null) {
-  console.log(name);
-  return function(target: Function) {
-    // const registry: Map<string, Function> = (<any>global).__SPRING_LIKE_CONTAINER__ || {};
-    // const dependency = registry.get(name);
-    // if (!dependency) {
-    //   registry[name] = target(name, target);
-    //   console.log(registry.get(name));
-    // }
+import { getSpringLikeRegistry } from "../util";
+
+export function AutoWire(name) {
+  return function(target, propertyKey: string) {
+    const registry = getSpringLikeRegistry();
+    if (registry[name]) {
+      registry[name][propertyKey] = registry[propertyKey];
+      console.log(name, "found", target[propertyKey]);
+    } else {
+      //  create it and store it inside registry then set it
+      console.log(name, "not found", target[propertyKey], registry[name]);
+    }
   };
 }
