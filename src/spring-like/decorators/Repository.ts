4338e873d3f27ff559ce6193674026d138461d98
Anand Patel <anandpatel3@hotmail.com>
2019-04-10 15:28:05 -0400

    Update Repository.ts

--- src/spring-like/decorators/Repository.ts
+++ src/spring-like/decorators/Repository.ts
@@ -1,21 +1,16 @@
+import { getSpringLikeRegistry, InstanceFactory } from "../util";
+
 export function Repository(name: string = null) {
   // console.log(name);
   return function(target: Function) {
-    (<any>global).__SPRING_LIKE_CONTAINER__ = (<any>global).__SPRING_LIKE_CONTAINER__ || {};
-    const registry = (<any>global).__SPRING_LIKE_CONTAINER__;
-    
+    const registry = getSpringLikeRegistry();
+
     const dependency = registry[name];
 
     if (!dependency) {
-      registry[name] = target;
-      registry[name] = Object.create(registry[name]) 
-
-      console.log(name, registry[name], target, typeof target);
-
-      // need to instantiate the class and hook up any autowired dependencies
-      // console.log(registry[name].length);
-      // console.log(registry[name]);
+      if (!registry[name]) {
+        registry[name] = InstanceFactory(target, []);
+      }
     }
-    
   };
 }
