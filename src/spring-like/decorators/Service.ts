eeacb6f21144c3b97fe40170db174f93408c0343
Anand Patel <anandpatel3@hotmail.com>
2019-04-10 15:28:33 -0400

    Update Service.ts

--- src/spring-like/decorators/Service.ts
+++ src/spring-like/decorators/Service.ts
@@ -1,16 +1,18 @@
+import { InstanceFactory } from "../util";
+
 export function Service(name: string = null) {
   // console.log(name);
   return function(target: Function) {
-    (<any>global).__SPRING_LIKE_CONTAINER__ = (<any>global).__SPRING_LIKE_CONTAINER__ || {};
+    (<any>global).__SPRING_LIKE_CONTAINER__ =
+      (<any>global).__SPRING_LIKE_CONTAINER__ || {};
     const registry = (<any>global).__SPRING_LIKE_CONTAINER__;
-    
+
     const dependency = registry[name];
 
     if (!dependency) {
-      registry[name] = target;
-      registry[name] = Object.create(registry[name]) 
-      console.log(name, registry[name], target, typeof target);
+      if (!registry[name]) {
+        registry[name] = InstanceFactory(target, []);
+      }
     }
-    
   };
 }
