949c5d39a460b3c7bd7a905ca1f560eb4397c6d7
Anand Patel <anandpatel3@hotmail.com>
2019-04-10 15:27:54 -0400

    Update Controller.ts

--- src/spring-like/decorators/Controller.ts
+++ src/spring-like/decorators/Controller.ts
@@ -1,23 +1,21 @@
+import { getSpringLikeRegistry, InstanceFactory } from "../util";
+
 export function Controller(name: string = null) {
   // console.log(name);
   return function(target: Function) {
+    const registry = getSpringLikeRegistry();
 
-    (<any>global).__SPRING_LIKE_CONTAINER__ = (<any>global).__SPRING_LIKE_CONTAINER__ || {};
-    const registry = (<any>global).__SPRING_LIKE_CONTAINER__;
-    
     const dependency = registry[name];
 
     if (!dependency) {
-      registry[name] = target;
-      registry[name] = Object.create(registry[name]) 
-      console.log(name, registry[name], target, typeof target);
+      if (!registry[name]) {
+        registry[name] = InstanceFactory(target, []);
+      }
 
       // console.log(registry[name].length);
       // console.log(registry[name]);
     }
 
     // console.log((<any>global).__SPRING_LIKE_CONTAINER__);
-    
-    
   };
 }
