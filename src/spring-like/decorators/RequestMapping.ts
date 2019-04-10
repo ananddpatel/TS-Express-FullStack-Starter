80df27068532694ffe769f8c5df3145266c448be
Anand Patel <anandpatel3@hotmail.com>
2019-04-10 15:28:23 -0400

    Update RequestMapping.ts

--- src/spring-like/decorators/RequestMapping.ts
+++ src/spring-like/decorators/RequestMapping.ts
@@ -1,16 +1,19 @@
-export function RequestMapping(path: string, type: 'get' | 'post') {
+import { getSpringLikeRequestMapping, getSpringLikeRegistry } from "../util";
 
-  const name = 'homeController'
-  
-  return function(target, propertyName: string, propertyDesciptor: PropertyDescriptor) {
-    (<any>global).__SPRING_LIKE_CONTAINER_REQUEST_MAPPINGS__ = (<any>global).__SPRING_LIKE_CONTAINER_REQUEST_MAPPINGS__ || new Map<string, [string, Function]>();
-    const reqMap = (<any>global).__SPRING_LIKE_CONTAINER_REQUEST_MAPPINGS__;
-  
+export function RequestMapping(path: string, type: "get" | "post") {
+  const name = "homeController";
+
+  return function(
+    target,
+    propertyName: string,
+    propertyDesciptor: PropertyDescriptor
+  ) {
+    const reqMap = getSpringLikeRequestMapping();
+
     const mapping = reqMap.get(path);
     if (!mapping) {
       // console.log(path, type, propertyDesciptor, target, propertyName);
-      (<any>global).__SPRING_LIKE_CONTAINER__ = (<any>global).__SPRING_LIKE_CONTAINER__ || {};
-      const registry = (<any>global).__SPRING_LIKE_CONTAINER__;
+      const registry = getSpringLikeRegistry();
 
       const dependency = registry[name];
 
@@ -21,5 +24,5 @@ export function RequestMapping(path: string, type: 'get' | 'post') {
       // look inside __SPRING_LIKE_CONTAINER__ for the intiatlized class and give that guy's method otherwise it wont know and will say things are undefined
       reqMap.set(path, [type, registry[name][propertyName]]);
     }
-  }
+  };
 }
